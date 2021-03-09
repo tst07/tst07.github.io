1cb0b79717a42231ce85ee5758c1a8b4383a369a
Prakhar Mishra <prakhar.mishra@airtel.com>
2021-03-10 00:31:02 +0530

    first commit

---
+++ content/hello_world.md
@@ -0,0 +1,11 @@
+Title: My first post
+Date: 2021-03-10 01:01
+Modified: 2021-03-10 01:01
+Category: misc
+Tags: first, misc
+Slug: My-first-post
+Authors: Prakhar Mishra
+Summary: This is what start of a great thing looks like.
+
+This is the *first post*. **YAY!**
+![Write here a good description for people who can't see the image]({static}/images/dark.jpg)
---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Prakhar Mishra'
+SITENAME = 'Prakhar Mishra'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Asia/Kolkata'
+STATIC_PATHS = ['images']
+DEFAULT_LANG = 'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = 'feeds/all.atom.xml'
+FEED_ALL_RSS = 'feeds/all.rss.xml'
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = 'feeds/%s.rss.xml'
+RSS_FEED_SUMMARY_ONLY = False
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
